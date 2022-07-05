2348d908043a3e1fb1dae9df8135b827b875b1b0
Igor Goloborodko <igor.goloborodko@gmail.com>
2022-07-05 14:35:51 +0300

    test2

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -6,7 +6,7 @@ import styles from "./Link.module.css";
 export function Link(props) {
   const { url, title, children } = props;
   return (
-    <DocLink className="button button--lg asdkfjklajs" to={url}>
+    <DocLink className="button button--lg akfjklajs" to={url}>
       <div className={styles.link}>
         {title}
         {children}
